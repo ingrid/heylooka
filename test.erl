e740decad427b4d38279d46af7b22201fb8e0fb6
Thomas Ballinger <thomasballinger@gmail.com>
2012-08-18 12:34:52 -0400

    Single chatroom prototype working

--- test.erl
+++ test.erl
@@ -7,6 +7,7 @@ hello() ->
     io:format("hello~p~p~p~n", [3, 4, 3]).
 
 start() ->
+    register(chat, spawn(?MODULE, chatroom, [[]])),
     register(tcp, spawn(?MODULE, listen, [])),
     timer:sleep(infinity).
 
@@ -19,25 +20,34 @@ listen(LSock) ->
     io:format("listen on a socket started~n"),
     {ok, Socket} = gen_tcp:accept(LSock),
     io:format("just accepted a socket~n"),
-    ClientResponder = spawn(?MODULE, print, [Socket]),
+    ClientResponder = spawn(?MODULE, client, [Socket, 0, 0]),
+    io:format("just spawned a client~n"),
     gen_tcp:controlling_process(Socket, ClientResponder),
+    io:format("just set client to use socket~n"),
+    chat ! {useradd, ClientResponder},
+    io:format("just told chatroom to add user~n"),
     listen(LSock).
 
-chatroom(Users) ->
+chatroom(UserPids) ->
     receive
-        {useradd, User} ->
-            {client, X, Y, Pid} = User,
-            io:format('added user ~s at ~i ~i', [Pid, X, Y]),
-            chatroom(Users ++ [User])
+        {useradd, UserPid} ->
+            io:format('added user ~w', [UserPid]),
+            chatroom(UserPids ++ [UserPid]);
+        {msg, From, Data} ->
+            F = fun(Pid) -> Pid ! {msg, From, Data} end,
+            lists:foreach(F, UserPids),
+            chatroom(UserPids)
     end.
 
-client(Socket) ->
+client(Socket, X, Y) ->
     receive
         {tcp, Socket, Data} ->
-            io:format('~n~preceived some data: ', self()),
+            io:format('~n~p received some data: ', [self()]),
             io:format(Data ++ "\n"),
-            client(Socket);
-        {msg, {client, X, Y, Pid}=From, Data} ->
-            gen_tcp:send(Socket, Data++"\n")
+            chat ! {msg, {client, X, Y, self()}, Data},
+            client(Socket, X, Y);
+        {msg, {client, From_X, From_Y, From_Pid}=From, Data} ->
+            io:format('~n~p relayed some data: ', [self()]),
+            gen_tcp:send(Socket, Data++"\n"),
+            client(Socket, X, Y)
     end.
-
