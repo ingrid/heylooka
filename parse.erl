dd9843a91e32c6a96163a3f8c50f8e090f42db87
Thomas Ballinger <thomasballinger@gmail.com>
2012-08-19 22:14:08 -0400

    correct encoding of outgoing messages

--- parse.erl
+++ parse.erl
@@ -7,13 +7,19 @@ main() ->
     test_irc_to_tuple("/event Boom"),
     test_extract_irc_from_raw("12 abcdefhgijkl4 nextUNPARSABLEREST"),
     test_extract_irc_from_raw("4 nextUNPARSABLEREST"),
-    test_extract_irc_from_raw("UNPARSABLEREST").
+    test_extract_irc_from_raw("UNPARSABLEREST"),
+    test_encode_tuple_to_raw(),
+    ok.
 
 test_irc_to_tuple(Str) ->
     io:format("result of irc_to_tuple(\"~s\"): ~w~n", [Str, irc_to_tuple(Str)]).
 
 test_extract_irc_from_raw(Str) ->
     io:format("result of extract_irc_from_raw(\"~s\"): ~w~n", [Str, extract_irc_from_raw(Str)]).
+
+test_encode_tuple_to_raw() ->
+    Full = {msg, {user, 4, 7, fake, "Tom"}, "Boom!", 17},
+    io:format("result of encode_irc_to_raw(~w): \"~s\"~n", [Full, encode_tuple_to_raw(Full)]).
 
 irc_to_tuple(Data) ->
     case Data of
@@ -48,6 +54,7 @@ extract_irc_from_raw(Data) ->
 encode_tuple_to_raw(Tuple) ->
     case Tuple of
         {msg, {user, From_X, From_Y, From_Pid, From_Name}, EventName, Radius} ->
-            Msg = io_lib:format("~w ~w ~w ~s", [Radius, From_X, From_Y, EventName]),
-            Msg
+            Msg = lists:flatten(io_lib:format("~w ~w ~w ~s", [Radius, From_X, From_Y, EventName])),
+            FullMsg = io_lib:format("~w ~s", [string:len(Msg), Msg]),
+            FullMsg
     end.
